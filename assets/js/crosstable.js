39867cf31725288c486710061d79f50c9acb4fe2
Kamil Kufel <116422453+Kkmil@users.noreply.github.com>
2025-01-31 22:01:30 +0100

    First and Last (🙏) Commit

---
+++ assets/js/crosstable.js
@@ -0,0 +1,69 @@
+
+window.onload = function() {
+    activateListener("player1");
+    activateListener("player2");
+}
+
+function run() {
+	let player1  = document.getElementById("player1").value.toLowerCase();
+    let player2  = document.getElementById("player2").value.toLowerCase();
+	let player1c = document.getElementById("player1").value;
+    let player2c = document.getElementById("player2").value;
+    let url = `https://lichess.org/api/crosstable/${player1}/${player2}`;
+
+    fetch(url)
+    	.then(response => response.json())
+    	.then(data => {
+            document.getElementById("player1name").innerText = player1c + ":";
+            document.getElementById("player2name").innerText = player2c + ":";
+            document.getElementById("totalgames").innerText = data.nbGames + " Games";
+
+            document.getElementById("player1wins").innerText = `${data.users[player1]} Wins`;
+            document.getElementById("player2wins").innerText = `${data.users[player2]} Wins`;
+            getPlayer1Status();
+            getPlayer2Status();
+    });
+}
+
+function activateListener(id) {
+    const element = document.getElementById(id);
+    element.addEventListener("keyup", (e) => {
+        if (e.key === "Enter") {
+            run();
+        }
+    });
+}
+
+function getPlayer1Status() {
+	let player  = document.getElementById("player1").value.toLowerCase();
+    let url = `https://lichess.org/api/user/${player}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            if (data.online) {
+                document.getElementById("player1name").innerText = data.username + "  🟢";
+            } else {
+                document.getElementById("player1name").innerText = data.username + "  🔴";
+            }
+			if (data.tosViolation) {
+				document.getElementById("player1name").innerText += "  🚫";
+            }
+        });
+}
+
+function getPlayer2Status() {
+    let player  = document.getElementById("player2").value.toLowerCase();
+    let url = `https://lichess.org/api/user/${player}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            if (data.online) {
+                document.getElementById("player2name").innerText = data.username + "  🟢";
+            } else {
+                document.getElementById("player2name").innerText = data.username + "  🔴";
+            }
+			if (data.tosViolation) {
+				document.getElementById("player2name").innerText += "  🚫";
+            }
+        });
+}
---
+++ assets/js/search.js
@@ -0,0 +1,104 @@
+
+window.onload = function() {
+    if (sessionStorage.getItem("name") != null) {
+        document.getElementById("searchbar").value = sessionStorage.getItem("name");
+        search();
+        sessionStorage.clear();
+    }
+	activateListener("indexsearchbar");
+	activateListener("searchbar");
+}
+
+
+function setColor(color) {
+    selectedColor = color;
+    fetchStats(document.getElementById("searchbar").value);
+}
+
+function search() {
+    let username = document.getElementById("searchbar").value;
+    let url = `https://lichess.org/api/user/${username}`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            // Update user data as before
+            if (data.online) {
+                document.getElementById("username").innerText = data.username + "  🟢";
+            } else {
+                document.getElementById("username").innerText = data.username + "  🔴";
+            }
+            if (data.tosViolation) {
+                document.getElementById("username").innerText += "  🚫";
+            }
+            document.getElementById("created_at").innerText = new Date(data.createdAt).toLocaleDateString();
+            document.getElementById("last_seen").innerText = new Date(data.seenAt).toLocaleDateString();
+            document.getElementById("playtime").innerText = Math.round(data.playTime.total / 60 / 60) + "h";
+            document.getElementById("wins").innerText = data.count.win;
+            document.getElementById("losses").innerText = data.count.loss;
+            document.getElementById("draws").innerText = data.count.draw;
+            document.getElementById("blitz_games").innerText = data.perfs.blitz.games;
+            document.getElementById("blitz_rating").innerText = data.perfs.blitz.rating;
+            if (data.perfs.blitz.games < 30) {
+                document.getElementById("blitz_rating").innerText += "?";
+            }
+            document.getElementById("bullet_games").innerText = data.perfs.bullet.games;
+            document.getElementById("bullet_rating").innerText = data.perfs.bullet.rating;
+            if (data.perfs.bullet.games < 10) {
+                document.getElementById("bullet_rating").innerText += "?";
+            }
+            document.getElementById("puzzle_games").innerText = "0";
+            document.getElementById("rapid_games").innerText = data.perfs.rapid.games;
+            document.getElementById("rapid_rating").innerText = data.perfs.rapid.rating;
+            if (data.perfs.rapid.games < 30) {
+                document.getElementById("rapid_rating").innerText += "?";
+            }
+            document.getElementById("puzzle_games").innerText = "0";
+            document.getElementById("puzzle_rating").innerText = "0";
+            document.getElementById("puzzle_games").innerText = data.perfs.puzzle.games;
+            document.getElementById("puzzle_rating").innerText = data.perfs.puzzle.rating;
+            document.getElementById("searchbar").value = "";
+
+            // Fetch stats with the selected color
+            fetchStats(username);
+        });
+}
+function fetchStats(username, selectedColor) {
+	var selectedColor = white;
+    let url = `https://explorer.lichess.ovh/player?player=${username}&color=$(selectedColor);
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            if (selectedColor === 'white') {
+                document.getElementById("white_games").innerText = data.white;
+            } else if (selectedColor === 'black') {
+                document.getElementById("black_games").innerText = data.black;
+            }
+        })
+        .catch(error => console.error('Error fetching stats:', error));
+}
+
+function searchHome() {
+    let name = document.getElementById("indexsearchbar").value;
+	sessionStorage.setItem("name", name);
+    location.replace("search.html");
+}
+
+function activateListener(id) {
+	try {
+		const element = document.getElementById(id);
+		element.addEventListener("keyup", (e) => {
+			if (id == "searchbar") {
+				if (e.key === "Enter") {
+					search();
+				}
+			} else {
+				if (e.key === "Enter") {
+					searchHome();
+				}
+			}
+		});
+	} catch (error) {
+	}
+}
